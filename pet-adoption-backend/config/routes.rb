f234f0b954b7d799938f9fc9fbf94f49c267a3ad
github username <github email address>
2019-11-12 17:28:14 -0600

    Surrender animal mostly done

--- pet-adoption-backend/config/routes.rb
+++ pet-adoption-backend/config/routes.rb
@@ -10,8 +10,8 @@ Rails.application.routes.draw do
   # end
   namespace :api do 
     namespace :v1 do 
-        resources :users, only: [:index,:show]
-        resources :animals, only: [:index,:show]
+        resources :users
+        resources :animals
     end
   end
 end
