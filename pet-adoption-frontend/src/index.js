ef784765cad1df548750ebad9767cde37afb7ba3
steven nguyen <51390418+sqnguyen829@users.noreply.github.com>
2019-11-12 22:23:57 -0600

    Update index.js

--- pet-adoption-frontend/src/index.js
+++ pet-adoption-frontend/src/index.js
@@ -34,100 +34,100 @@ document.addEventListener('DOMContentLoaded', () => {
     
     // }
 
-surTab.addEventListener("click", ()=>{
-    hideList = !hideList
-    if (hideList) {
-        animalList.style.display = 'none'
+    surTab.addEventListener("click", ()=>{
+        hideList = !hideList
+        if (hideList) {
+            animalList.style.display = 'none'
 
-        const form = document.createElement("form")
-        const inputAnimalName = document.createElement('input')
-        const inputSpecies = document.createElement('input')
-        const inputBreed = document.createElement('input')
-        const inputImage = document.createElement('input')
-        const inputGender = document.createElement('input')
-        const inputAge = document.createElement('input')
-        const inputDesc = document.createElement('input')
-        const surSubmit = document.createElement('button')
+            const form = document.createElement("form")
+            const inputAnimalName = document.createElement('input')
+            const inputSpecies = document.createElement('input')
+            const inputBreed = document.createElement('input')
+            const inputImage = document.createElement('input')
+            const inputGender = document.createElement('input')
+            const inputAge = document.createElement('input')
+            const inputDesc = document.createElement('input')
+            const surSubmit = document.createElement('button')
 
-        const br1 = document.createElement('br')
-        const br2 = document.createElement('br')
-        const br3 = document.createElement('br')
-        const br4 = document.createElement('br')
-        const br5 = document.createElement('br')
-        const br6 = document.createElement('br')
-        const br7 = document.createElement('br')
+            const br1 = document.createElement('br')
+            const br2 = document.createElement('br')
+            const br3 = document.createElement('br')
+            const br4 = document.createElement('br')
+            const br5 = document.createElement('br')
+            const br6 = document.createElement('br')
+            const br7 = document.createElement('br')
 
-        inputAnimalName.value = ""
-        inputAnimalName.placeholder = "Enter a name..."
+            inputAnimalName.value = ""
+            inputAnimalName.placeholder = "Enter a name..."
 
-        inputSpecies.value = ""
-        inputSpecies.placeholder = "Enter a Species..."
+            inputSpecies.value = ""
+            inputSpecies.placeholder = "Enter a Species..."
 
-        inputBreed.value = ""
-        inputBreed.placeholder = "Enter a breed..."
+            inputBreed.value = ""
+            inputBreed.placeholder = "Enter a breed..."
 
-        inputImage.value = ""
-        inputImage.placeholder = "Enter a imgae url..."
+            inputImage.value = ""
+            inputImage.placeholder = "Enter a imgae url..."
 
-        inputGender.value = ""
-        inputGender.placeholder = "Enter a gender..."
+            inputGender.value = ""
+            inputGender.placeholder = "Enter a gender..."
 
-        inputAge.value = ""
-        inputAge.placeholder = "Enter a age..."
+            inputAge.value = ""
+            inputAge.placeholder = "Enter a age..."
 
-        inputDesc.value = ""
-        inputDesc.placeholder = "Enter a description..."
+            inputDesc.value = ""
+            inputDesc.placeholder = "Enter a description..."
 
-        surSubmit.type = "submit"
-        surSubmit.name = "submit"
+            surSubmit.type = "submit"
+            surSubmit.name = "submit"
 
-        surSubmit.innerText = "Surrender Animal"
-        surSubmit.className = "submit"
-        form.addEventListener('submit', ()=>{
-            event.preventDefault()
-            // debugger 
-            let animalName = event.target[0].value
-            let species = event.target[1].value
-            let breed = event.target[2].value
-            let image = event.target[3].value
-            let gender = event.target[4].value
-            let age = event.target[5].value
-            let desc = event.target[6].value
-            form.reset()
-            fetch(animalURL, {
-                 method: "POST",
-                 headers: {
-                     "Content-Type": "application/json"
-                 },
-                 body: JSON.stringify({
-                     "animal":{
-                        "name": animalName,
-                        "species": species,
-                        "breed": breed,
-                        "image": image,
-                        "gender": gender,
-                        "age": age,
-                        "status": "Availible",
-                        "description": desc}
-                 })
+            surSubmit.innerText = "Surrender Animal"
+            surSubmit.className = "submit"
+            form.addEventListener('submit', ()=>{
+                event.preventDefault()
+                // debugger 
+                let animalName = event.target[0].value
+                let species = event.target[1].value
+                let breed = event.target[2].value
+                let image = event.target[3].value
+                let gender = event.target[4].value
+                let age = event.target[5].value
+                let desc = event.target[6].value
+                form.reset()
+                fetch(animalURL, {
+                    method: "POST",
+                    headers: {
+                        "Content-Type": "application/json"
+                    },
+                    body: JSON.stringify({
+                        "animal":{
+                            "name": animalName,
+                            "species": species,
+                            "breed": breed,
+                            "image": image,
+                            "gender": gender,
+                            "age": age,
+                            "status": "Availible",
+                            "description": desc}
+                    })
+                })
+                .then(res => res.json())
+                .then(newAnimal => {
+                    console.log(newAnimal)
+                })
             })
-            .then(res => res.json())
-            .then(newAnimal => {
-                console.log(newAnimal)
-         })
-        })
 
-        form.append(inputAnimalName,br1,inputSpecies,br2,inputBreed,br3,inputImage,br4,inputGender,br5,inputAge,br6,inputDesc,br7,surSubmit)
-        surrenderDiv.append(form)
+            form.append(inputAnimalName,br1,inputSpecies,br2,inputBreed,br3,inputImage,br4,inputGender,br5,inputAge,br6,inputDesc,br7,surSubmit)
+            surrenderDiv.append(form)
     
-    }else {
-        while(surrenderDiv.firstChild){
-            surrenderDiv.removeChild(surrenderDiv.firstChild)
+        }else {
+            while(surrenderDiv.firstChild){
+                surrenderDiv.removeChild(surrenderDiv.firstChild)
+            }
+            animalList.style.display = 'block'
+            console.log("show list")
         }
-        animalList.style.display = 'block'
-        console.log("show list")
-    }
-})
+    })
 
 // Fernando - Login form
 
