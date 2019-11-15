b17552edb6ed75d4a40b54472a447940d9101e8d
github username <github email address>
2019-11-15 14:53:43 -0600

    presentable

--- pet-adoption-frontend/src/index.js
+++ pet-adoption-frontend/src/index.js
@@ -6,6 +6,8 @@ const surTab = document.querySelector('#surrender')
 const surSubmit = document.createElement('button')
 const animalList = document.querySelector('#animal-list')
 const surrenderDiv = document.querySelector('#surrenderDiv')
+const myPetDiv = document.querySelector('#myPetDiv')
+const animalListingDiv = document.querySelector('#animalListingDiv')
 
     fetch(animalURL)
     .then(res => res.json())
@@ -16,7 +18,8 @@ const surrenderDiv = document.querySelector('#surrenderDiv')
 
     ///////////////// show animal list start /////////////////////////////////////
 function showAnimals(animal){
-    if(animal.status === "Availible"){
+
+    if(animal.status === "Available"){
         const animalCard = document.createElement('div')
         animalCard.className = "card h-100"
         animalCard.id = animal.id
@@ -72,13 +75,11 @@ function showAnimals(animal){
     
         const aTag = document.createElement('a')
         aTag.setAttribute("href", "#")
-        aTag.className = "btn btn-primary"
+        aTag.className = "btn btn-success"
         aTag.innerText = "Find Out More!"
         aTag.id = animal.id
         
         //////////////////////// Posting new Listing start //////////////////////
-        
-        const animalListingDiv = document.querySelector('#animalListingDiv')
     
         aTag.addEventListener('click', ()=>{
     
@@ -157,6 +158,7 @@ function showAnimals(animal){
                 })
                 .then(res => res.json())
                 .then(addListed => {
+                    // .animals.push(addListed)
                     console.log(addListed)
                 })
     
@@ -172,11 +174,15 @@ function showAnimals(animal){
                 })
                 .then(res => res.json())
                 .then(updateAnimal =>{
-                    animal = updateAnimal
+                    // animal = updateAnimal
                     ////this is to update the new status for the animal
-                    //status.innerText = animal.status
+                    showAnimals(updateAnimal)
                 })
                 while(animalListingDiv.firstChild){
+                    animalListingDiv.removeChild(animalListingDiv.firstChild)
+                }
+
+                while (animalListingDiv.firstChild) {
                     animalListingDiv.removeChild(animalListingDiv.firstChild)
                 }
                 animalList.style.display = 'block'
@@ -202,6 +208,15 @@ function showAnimals(animal){
 
  //////////////////////// Posting new animals start //////////////////////
     surTab.addEventListener("click", ()=>{
+
+        while(myPetDiv.firstChild){
+            myPetDiv.removeChild(myPetDiv.firstChild)
+        }
+
+        while (animalListingDiv.firstChild) {
+            animalListingDiv.removeChild(animalListingDiv.firstChild)
+        }
+
         const form = document.createElement('form')
         console.log('clicked')
         hideList = !hideList
@@ -298,120 +313,282 @@ function showAnimals(animal){
     })
 
     ////////////////////////   Sign In Starts Here  //////////////////////////////////////////
-    const signInForm = document.querySelector('#sign-in-form')
+const signInForm = document.querySelector('#sign-in-form')
 
-    signInForm.addEventListener('submit', () => {
-        event.preventDefault()
-
-
-        fetch('http://localhost:3000/api/v1/users')
-
-            .then(res => res.json())
-            .then(users => {
-                // users.forEach(data => displayUserOnNavBar(data))
-                displayUserOnNavBar(users)
-            })
-
-        function displayUserOnNavBar(user) {
-            let chosenUser = user.filter(u => u.user_name === signInForm[0].value)
-            console.log(chosenUser)
-            const loginBarNameIl = document.querySelector('#login-bar-name')
-
-            if (chosenUser.length == 1) {
-                const pUsername = document.createElement('p')
-                const dropdownFormSignIn = document.querySelector('#dropdownFormSignIn')
-                const navbarDropdownMenuLink = document.querySelector('#navbarDropdownMenuLink')
-                const navbarDropdownMenuLinkSignUp = document.querySelector('#navbarDropdownMenuLinkSignUp')
-                pUsername.className = 'nav-link'
-
-                const spanUsername = document.createElement('span')
-                /////////////////////////the tag need an ID//////////////////////////////
-                spanUsername.id = chosenUser[0].id
-                /////////////////////////////////////////////////////////////////////
-                spanUsername.innerText = 'Hello, ' + chosenUser[0].first_name + '!'
-                spanUsername.className = 'text-success'
-
-                pUsername.append(spanUsername)
-                loginBarNameIl.append(pUsername)
-
-                dropdownFormSignIn.style.display = "none";
-                navbarDropdownMenuLink.style.display = "none";
-                navbarDropdownMenuLinkSignUp.style.display = "none";
-
-                const pLoggout = document.createElement('p')
-                pLoggout.className = 'nav-link'
-                pLoggout.innerText = 'Log Out'
-
-                const loginBarLogout = document.querySelector('#login-bar-logout')
-                loginBarLogout.append(pLoggout)
-
-                loginBarLogout.addEventListener('click', () => {
-                    // window.location.reload(false); 
-                    loginBarNameIl.innerHTML = ""
-                    navbarDropdownMenuLink.style.display = "block";
-                    navbarDropdownMenuLinkSignUp.style.display = "block";
-                    // dropdownFormSignIn.style.display = "block";
-                    loginBarLogout.innerHTML = ""
-
-                    navbarDropdownMenuLink.addEventListener('click', () => {
-                        dropdownFormSignIn.style.display = "block";
-                    })
+signInForm.addEventListener('submit', () => {
+    event.preventDefault()
 
 
+    fetch('http://localhost:3000/api/v1/users')
+
+        .then(res => res.json())
+        .then(users => {
+            // users.forEach(data => displayUserOnNavBar(data))
+            displayUserOnNavBar(users)
+        })
+
+    function displayUserOnNavBar(user) {
+        let chosenUser = user.filter(u => u.user_name === signInForm[0].value)
+        // console.log(chosenUser)
+        const loginBarNameIl = document.querySelector('#login-bar-name')
+
+        if (chosenUser.length == 1) {
+            const pUsername = document.createElement('p')
+            const dropdownFormSignIn = document.querySelector('#dropdownFormSignIn')
+            const navbarDropdownMenuLink = document.querySelector('#navbarDropdownMenuLink')
+            const navbarDropdownMenuLinkSignUp = document.querySelector('#navbarDropdownMenuLinkSignUp')
+            pUsername.className = 'nav-link'
+
+            const spanUsername = document.createElement('span')
+            spanUsername.id = chosenUser[0].id
+            spanUsername.innerText = 'Hello, ' + chosenUser[0].first_name + '!'
+            spanUsername.className = 'text-success'
+
+            pUsername.append(spanUsername)
+            loginBarNameIl.append(pUsername)
+
+            dropdownFormSignIn.style.display = "none";
+            navbarDropdownMenuLink.style.display = "none";
+            navbarDropdownMenuLinkSignUp.style.display = "none";
+
+            const pLoggout = document.createElement('p')
+            pLoggout.className = 'nav-link'
+            pLoggout.innerText = 'Log Out'
+
+            const loginBarLogout = document.querySelector('#login-bar-logout')
+            loginBarLogout.append(pLoggout)
+
+            const ulNavaBar = document.querySelector('#ulNavaBar')
+            // <li class="nav-item">
+            //     <a class="nav-link" href="#">My Pets</a>
+            // </li> 
+
+            const liNavaBar = document.createElement('li')
+            liNavaBar.className = 'nav-item'
+            
+////////////////////////////////////////////My pets start///////////////////////////////
+
+            liNavaBar.addEventListener('click', () => {
+                animalList.style.display = 'none'
+                console.log(user)
+
+                while(myPetDiv.firstChild){
+                    myPetDiv.removeChild(myPetDiv.firstChild)
+                }
+
+                while (surrenderDiv.firstChild) {
+                    surrenderDiv.removeChild(surrenderDiv.firstChild)
+                }
+
+                while (animalListingDiv.firstChild) {
+                    animalListingDiv.removeChild(animalListingDiv.firstChild)
+                }
+
+                let loggedInCurrentUser = document.querySelector('.text-success')
+                console.log(loggedInCurrentUser.id)
+                console.log(user.filter(data => data.id == loggedInCurrentUser.id))
+                let listOfUserInfo = user.filter(data => data.id == loggedInCurrentUser.id)
+
+                let userAnimals = listOfUserInfo[0].animals
+                
+                userAnimals.forEach(animal => {
+
+                    const myDivCol = document.createElement('div')
+                    myDivCol.className = "col-lg-3 col-md-6 mb-4"
+
+                    const myAnimalCard = document.createElement('div')
+                    myAnimalCard.className = "card h-100"
+                    myAnimalCard.id = animal.id
+
+                    const myAnimalImg = document.createElement('img')
+                    myAnimalImg.src = animal.image
+                    myAnimalImg.className = "card-img-top"
+                    myAnimalImg.setAttribute("alt", " ")
+
+                    const myBodyDiv = document.createElement('div')
+                    myBodyDiv.className = "card-body"
+
+                    const myH4 = document.createElement('h4')
+                    myH4.className = "card-title"
+                    myH4.innerText = animal.name 
+
+                    const myUl = document.createElement('ul')
+                    myUl.setAttribute("style", "text-align: left;")
+
+                    const myLiSpecie = document.createElement('li')
+                    myLiSpecie.id = "specie"
+
+                    const mySpanSpecie = document.createElement('span')
+                    mySpanSpecie.innerText = `Specie: ${animal.species}`
+
+                    const myLiGender = document.createElement('li')
+                    myLiGender.id = "gender"
+
+                    const mySpanGender = document.createElement('span')
+                    mySpanGender.innerText = `Gender: ${animal.gender}`
+
+                    const myLiAge = document.createElement('li')
+                    myLiAge.id = "age"
+                    
+                    const mySpanAge = document.createElement('span')
+                    mySpanAge.innerText = `Age: ${animal.age}`
+
+                    const myLiStatus = document.createElement('li')
+                    myLiStatus.id = "status"
+                    
+                    const mySpanStatus = document.createElement('span')
+                    mySpanStatus.innerText = `Status: ${animal.status}`
+
+                    myLiSpecie.append(mySpanSpecie)
+                    myLiGender.append(mySpanGender)
+                    myLiAge.append(mySpanAge)
+                    myLiStatus.append(mySpanStatus)
+
+                    myUl.append(myLiSpecie,myLiGender,myLiAge,mySpanStatus)
+
+                    myBodyDiv.append(myH4,myUl)
+
+                    myAnimalCard.append(myAnimalImg,myBodyDiv)
+
+                    myDivCol.append(myAnimalCard)
+
+                    myPetDiv.append(myDivCol)
+                
+                })
+            })//////////////////////////My pets end///////////////////////////////
+
+            const aNavaBar = document.createElement('a')
+            aNavaBar.className = 'nav-link'
+            aNavaBar.innerText = 'My Pets'
+            
+            liNavaBar.append(aNavaBar)
+            
+            ulNavaBar.insertBefore(liNavaBar, ulNavaBar.children[1])
+        
+            loginBarLogout.addEventListener('click', () => {
+                // window.location.reload(false); 
+                loginBarNameIl.innerHTML = ""
+                navbarDropdownMenuLink.style.display = "block";
+                navbarDropdownMenuLinkSignUp.style.display = "block";
+                // dropdownFormSignIn.style.display = "block";
+                loginBarLogout.innerHTML = ""
+
+                navbarDropdownMenuLink.addEventListener('click', () => {
+                    dropdownFormSignIn.style.display = "block";
                 })
 
-                
-            } else {
-                const errorMessageForm = document.querySelector('#error-message-form')
+                liNavaBar.style.display = 'none'
 
-                const pFormSingIn = document.createElement('p')
-                pFormSingIn.innerText = "Wrong Username"
-                pFormSingIn.className = "text-danger"
 
-                errorMessageForm.append(pFormSingIn)
+            })
 
-            }
+
+        } else {
+            const errorMessageForm = document.querySelector('#error-message-form')
+
+            const pFormSingIn = document.createElement('p')
+            pFormSingIn.innerText = "Wrong Username"
+            pFormSingIn.className = "text-danger"
+
+            errorMessageForm.append(pFormSingIn)
+
         }
-    })////////////// Sign In Finshes Here /////////// 
+
+
+    }
+})////////////// Sign In Finshes Here /////////// 
+
 
     //////////////// Sign Up Starts Here /////////////
 
-    const singupFormDropdown = document.querySelector('#sigupUser')
-    const urlUsers = 'http://localhost:3000/api/v1/users/'
-    const formSigUpClassDropdown = document.querySelector('#formSigUpClassDropdown')
+const singupFormDropdown = document.querySelector('#sigupUser')
+const urlUsers = 'http://localhost:3000/api/v1/users/'
+const formSigUpClassDropdown = document.querySelector('#formSigUpClassDropdown')
 
-    singupFormDropdown.addEventListener('submit', () => {
-        event.preventDefault()
-        // debugger 
-        let userNameForm = event.target[0].value
-        let firstNameForm = event.target[1].value
-        let lastNameForm = event.target[2].value
-        let emailForm = event.target[3].value
-        let addressForm = event.target[4].value
-        let phoneForm = event.target[5].value
-        fetch(urlUsers, {
-            method: "POST",
-            headers: {
-                "Content-Type": "application/json"
-            },
-            body: JSON.stringify({
-                "user": {
-                    "user_name": userNameForm,
-                    "first_name": firstNameForm,
-                    "last_name": lastNameForm,
-                    "email": emailForm,
-                    "address": addressForm,
-                    "phone_num": phoneForm
-                }
-            })
+singupFormDropdown.addEventListener('submit', () => {
+    event.preventDefault()
+    // debugger 
+    let userNameForm = event.target[0].value
+    let firstNameForm = event.target[1].value
+    let lastNameForm = event.target[2].value
+    let emailForm = event.target[3].value
+    let addressForm = event.target[4].value
+    let phoneForm = event.target[5].value
+    fetch(urlUsers, {
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json"
+        },
+        body: JSON.stringify({
+            "user": {
+                "user_name": userNameForm,
+                "first_name": firstNameForm,
+                "last_name": lastNameForm,
+                "email": emailForm,
+                "address": addressForm,
+                "phone_num": phoneForm
+            }
         })
-        event.target.reset()
-        formSigUpClassDropdown.style.display = 'none';
-        dropdownFormSignIn.style.display = 'block';
+    })
+    event.target.reset()
+    formSigUpClassDropdown.style.display = 'none';
+    dropdownFormSignIn.style.display = 'block';
 
-        const messageToSigIn = document.createElement('p')
-        messageToSigIn.innerText = 'Plase Type Your User Name To Log In'
+    const messageToSigIn = document.createElement('p')
+    messageToSigIn.innerText = 'Plase Type Your User Name To Log In'
+    
 
-        dropdownFormSignIn.append(messageToSigIn)
+    dropdownFormSignIn.append(messageToSigIn)
 
-    })//////////////// Sign Up Ends Here /////////////
+    const navbarNavDropdown = document.querySelector('#navbarNavDropdown')
+
+
+    // <li class="nav-item">
+    //     <a class="nav-link" href="#">My Pets</a>
+    // </li>
+
+})//////////////// Sign Up Ends Here /////////////
+
+/////////////////////////Redirect to the index when logo or home is cliked ///////////////////////
+
+
+const homeNav = document.querySelector('#homeNav')
+const logoNav = document.querySelector('#logoNav')
+
+homeNav.addEventListener('click', () => {
+    console.log('click on the nav bar')
+    animalList.style.display = 'block'
+    animalList.removeAttribute("style")
+
+    while(myPetDiv.firstChild){
+        myPetDiv.removeChild(myPetDiv.firstChild)
+    }
+
+    while (surrenderDiv.firstChild) {
+        surrenderDiv.removeChild(surrenderDiv.firstChild)
+    }
+
+    while (animalListingDiv.firstChild) {
+        animalListingDiv.removeChild(animalListingDiv.firstChild)
+    }
+})
+
+logoNav.addEventListener('click', () => {
+    console.log('click on the nav bar')
+    animalList.style.display = 'block'
+    animalList.removeAttribute("style")
+
+    while(myPetDiv.firstChild){
+        myPetDiv.removeChild(myPetDiv.firstChild)
+    }
+
+    while (surrenderDiv.firstChild) {
+        surrenderDiv.removeChild(surrenderDiv.firstChild)
+    }
+
+    while (animalListingDiv.firstChild) {
+        animalListingDiv.removeChild(animalListingDiv.firstChild)
+    }
+})
+
+/////////////////////////Redirect to the index when logo or home is cliked finish ///////////////////////
